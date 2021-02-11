9b1ef51b7c95df010c646731c33733ee3def7b34
ryanmcd <rp.mcdan@gmail.com>
2021-02-11 00:46:42 -0500

    randomize tabs

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
+
+
 // grabs all tabs in current window (whichever I access the extension from)
 // tabs is an array of all tabs in the current window. property is called title.
+const tabsListing = []
+
 chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
   let tabsList = document.getElementById('tabs');
 
   for (let i = 0; i < tabs.length; i++) {
-    // console.log('TABS INPUT:', tabs);
+    console.log('TABS INPUT:', tabs);
     // console.log('TABS[i]:', tabs[i]);
-
-    let tabLink = tabs[i].url;
+    // tabsListing.push(tabs[i])
+    let tabLink = tabs[i].id;
     let tabTitle = tabs[i].title;
     // console.log(tabLink);
     // console.log(typeof(tabLink));
@@ -17,24 +21,21 @@ chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
     tabsList.appendChild(link);
   }
 
-  function onUpdated(tabs) {
-    console.log(`Updated tab: ${tabs.id}`);
-  }
-
-  function updateTab() {
-    let updating = chrome.tabs.update(tabs[0].id, {
-        active: true
+  //function to be invoked by eventlistener when the link is clicked on the chrome extension
+  function updateTab(tabID) {
+    let updating = chrome.tabs.update(tabID, {
+      active: true
     });
-    // updating.then(onUpdated);
   }
 
-  function updateTab() {
-    let updating = chrome.tabs.update(tabs[0].id, {
-        active: true
-    });
-    // updating.then(onUpdated);
+
+  for (let i = 0; i < tabs.length; i++) {
+    chrome.tabs.move(tabs[i].id, {index: Math.floor(Math.random() * (tabs.length - 0))})
   }
+  
 
-  updateTab()
-
+  const button = document.createElement("BUTTON")
+  button.innerHTML = 'CLICK ME!';
+  tabsList.appendChild(button);
+  
 });
